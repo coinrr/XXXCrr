3595af9e24fcb9516ec9790712e30720b275ee92
Jackal Lee <jackcakal@gmail.com>
2018-12-28 21:46:43 +0800

    First commit: download mvc template with auth

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Crr.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
